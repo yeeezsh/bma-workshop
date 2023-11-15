86bb3d1962203b8b200b14906c9bfa3959fc5ed4
kthuswongsa <korbboon.thuswongsa@agoda.com>
2023-11-15 16:12:48 +0700

    add 2c completed

--- 2_react/src/GameCard.tsx
+++ 2_react/src/GameCard.tsx
@@ -1,24 +1,29 @@
+import { useState } from 'react';
+
 interface GameCardProps {
-    title: string;
-    rating: number;
-    price: number;
-    image: string;
-  }
-  
-  const GameCard = (props: GameCardProps) => {
-    return (
-      <div className="gamecard">
-        <div className="gamecard__image">
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className="gamecard__info">
-          <h2 className="gamecard__title">{props.title}</h2>
-          <h4 className="gamecard__rating">{props.rating} / 5</h4>
-          <div className="gamecard__price">${props.price}</div>
-          <div className="gamecard__button"><button>Buy</button></div>
+  title: string;
+  rating: number;
+  price: number;
+  image: string;
+}
+
+const GameCard = (props: GameCardProps) => {
+  const [isBought, setIsBought] = useState(false);
+  return (
+    <div className={`gamecard ${isBought ? 'gamecard--bought' : ''}`}>
+      <div className="gamecard__image">
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className="gamecard__info">
+        <h2 className="gamecard__title">{props.title}</h2>
+        <h4 className="gamecard__rating">{props.rating} / 5</h4>
+        <div className="gamecard__price">${props.price}</div>
+        <div className="gamecard__button">
+          <button onClick={() => setIsBought(true)}>Buy</button>
         </div>
       </div>
-    );
-  };
-  
-  export default GameCard;
+    </div>
+  );
+};
+
+export default GameCard;
