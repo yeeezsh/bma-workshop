5f36e6a4daffea5705468e6214395b2ad5741a9d
kthuswongsa <korbboon.thuswongsa@agoda.com>
2023-11-16 12:18:43 +0700

    fix upload es import

--- 2_react/upload.js
+++ 2_react/upload.js
@@ -1,22 +1,22 @@
 // // thanks to ChatGpt 3.5
 // // prompt: I need node.js with no 3rd party lib that can upload all files in /dist (which is build web site file, including html and js) to specific endpoint. its cli program that accept 2 args, 1. is for endpoint 2. is for subdomain which will send attached in header
 
-const fs = require('fs');
-const http = require('http');
-const path = require('path');
+import { readdirSync, readFileSync } from 'fs';
+import { request } from 'http';
+import { join, basename } from 'path';
 
 // Function to read all files in a directory and return an array of file paths
 function getFiles(dir) {
-    const files = fs.readdirSync(dir, { withFileTypes: true });
-    return files.map(file => path.join(dir, file.name));
+    const files = readdirSync(dir, { withFileTypes: true });
+    return files.map(file => join(dir, file.name));
 }
 
 // Function to simulate file upload via POST request
 function simulateFileUpload(filePath, endpoint, subdomain) {
-    const fileContent = fs.readFileSync(filePath);
+    const fileContent = readFileSync(filePath);
 
     const boundary = '------WebKitFormBoundary1234567890'; // Change the boundary as needed
-    const data = `--${boundary}\r\nContent-Disposition: form-data; name="file"; filename="${path.basename(filePath)}"\r\nContent-Type: application/octet-stream\r\n\r\n${fileContent}\r\n--${boundary}--\r\n`;
+    const data = `--${boundary}\r\nContent-Disposition: form-data; name="file"; filename="${basename(filePath)}"\r\nContent-Type: application/octet-stream\r\n\r\n${fileContent}\r\n--${boundary}--\r\n`;
 
     const options = {
         hostname: endpoint,
@@ -30,7 +30,7 @@ function simulateFileUpload(filePath, endpoint, subdomain) {
         },
     };
 
-    const req = http.request(options, res => {
+    const req = request(options, res => {
         let responseData = '';
 
         res.on('data', chunk => {
@@ -59,7 +59,7 @@ if (process.argv.length !== 4) {
 
 const endpoint = process.argv[2];
 const subdomain = process.argv[3];
-const distPath = path.join(__dirname, './src/dist');
+const distPath = join(__dirname, './src/dist');
 
 // Get all files in the /dist directory
 const files = getFiles(distPath);
